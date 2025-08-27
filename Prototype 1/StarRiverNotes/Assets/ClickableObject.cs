2fd2ddee59cf4e23b1d0f2f99f41f497d76a359f
Dandyspyke <160555048+Dandyspyke@users.noreply.github.com>
2025-08-28 00:30:48 +1000

    0.3
    0.3

--- Prototype 1/StarRiverNotes/Assets/ClickableObject.cs
+++ Prototype 1/StarRiverNotes/Assets/ClickableObject.cs
@@ -5,12 +5,18 @@ public class ClickableObject : MonoBehaviour
 {
     public string st;
     public Text tt;
+    public InputField ln;
     void OnMouseDown()
     {
         ConnectionManager.Instance.OnObjectClicked(gameObject);
         Debug.LogError(transform.name);
-        tt.text = st;
 
-
+        tt.text = PlayerPrefs.GetString(st);
+        ln.onValueChanged.RemoveAllListeners();
+        ln.onValueChanged.AddListener((value) =>
+        {
+           tt.text = value;
+           PlayerPrefs.SetString(st,value);
+        });
     }
 }
